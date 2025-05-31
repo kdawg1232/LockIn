c6091c87085ccda4a40aced4558f170c97ee53c9
Karthik Digavalli <karthik@karthiks-air.myfiosgateway.com>
2025-05-31 11:08:03 -0400

    deleted disabled tests

--- __tests_disabled__/src_tests/setup.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Test setup configuration
- * Sets up environment variables and any necessary mocks
- */
-
-process.env.EXPO_PUBLIC_SUPABASE_URL = 'https://test.supabase.co';
-process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY = 'test-key'; 
